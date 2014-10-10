ee22832464f1b4aaea3717492f293f6846fccf15
yanshuoH <hys235606751@gmail.com>
2014-10-10 11:44:23 +0200

    add projectmodel, rewrite path validation in utils

--- models/ProjectModel.js
+++ models/ProjectModel.js
@@ -1,4 +1,4 @@
-// models/TaskModel.js
+// models/ProjecdtModel.js
 var utils = require('../lib/utils');
 var mongoose = require('mongoose');
 // For shortcut
@@ -7,44 +7,45 @@ var ObjectId = mongoose.Schema.ObjectId;
 /**
  * Schema Tree
  */
-var TaskModelSchema = new mongoose.Schema({
-    user: {type: ObjectId, ref: 'user'},
-    project: {type: ObjectId, ref: 'project'},
-    ref: {type: String, trim: true},
+var ProjectModelSchema = new mongoose.Schema({
+    creator: {type: ObjectId, ref: 'user'},
+    admin: [{
+        type: ObjectId, ref: 'user'
+    }],
+    users: [{
+        type: ObjectId, ref: 'user'
+    }],
+    tasks: [{
+        type: ObjectId, ref: 'task'
+    }],
     title: {type: String, trim: true},
-    priority: {type: Number, default: 0},
-    category: {
-        id: {type: ObjectId},
-        name: {type: String, trim: true}
-    },
     description: {type: String},
-    media: {},
+    ref: {type: String, trim: true},
     date: {
         created: {type: Date, default: Date.now},
-        updated: {type: Date, default: Date.now},
-    },
-    time: {
-        estimation: {type: Number},
-        consomation: {type: Number}
+        updated: {type: Date, default: Date.now}
     },
     status: {type: Number, default: 1}
 });
+
 
 /**
  * Validations
  */
 var requiredFields = [
-    // 'user',
+    // 'creator'
+    // 'admin'
+    // 'users',
     // 'project',
-    // 'ref',
+    'ref',
     'title',
     'description',
 ];
 // Passing schema by ref, add required field validation
-utils.addRequiredValidation(TaskModelSchema, requiredFields);
+utils.addRequiredValidation(ProjectModelSchema, requiredFields);
 
 // Limitation of title length
-TaskModelSchema.path('title').validate(function(title) {
+ProjectModelSchema.path('title').validate(function(title) {
     // 50 limitation
     if (typeof(title) !== 'undefined' && title.length > 50) {
         return false;
@@ -54,10 +55,11 @@ TaskModelSchema.path('title').validate(function(title) {
     }
 }, 'Invalid title - title is too long (max.50)');
 
+
 /**
  * Pre save
  */
-TaskModelSchema.pre('save', function(next) {
+ProjectModelSchema.pre('save', function(next) {
     // update updated date
     this.date.updated = Date.now();
     next();
@@ -67,14 +69,14 @@ TaskModelSchema.pre('save', function(next) {
  * Static functions
  * Heritage basic functions
  */
-TaskModelSchema.statics = utils.modelStatics;
+ProjectModelSchema.statics = utils.modelStatics;
 
 
 /**
  * Ending part
  */
 // Exports schema if needed
-exports.TaskModelSchema = TaskModelSchema;
+exports.ProjectModelSchema = ProjectModelSchema;
 // Build in mongo
-mongoose.model('TaskModel', TaskModelSchema);
-exports.TaskModel = mongoose.model('TaskModel');
+mongoose.model('ProjectModel', ProjectModelSchema);
+exports.ProjectModel = mongoose.model('ProjectModel');
