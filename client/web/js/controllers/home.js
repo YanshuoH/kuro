c7518024f314026c5d41acf02c60f8a2da35f629
YanshuH <hys235606751@gmail.com>
2014-12-07 12:44:43 +0100

    edit in place directive test

--- client/web/js/controllers/home.js
+++ client/web/js/controllers/home.js
@@ -7,10 +7,8 @@ kuroApp.controller('HomeCtrl', function($scope, $http) {
 });
 
 kuroApp.controller('TestCtrl', function($scope, $modal, $templateCache, apiService) {
-    $scope.open = function () {
-        var modalInstance = $modal.open({
-            templateUrl: 'testModal',
-            controller: 'TestCtrl',
-        })
-    }
+  $scope.contacts = [
+    { name: 'Sharon'},
+    { name: 'Steve'}
+  ];
 });
